87715b131a8e67a774f5986d59c39b1bfccd716d
alison <adevpr11@gmail.com>
2025-01-05 20:30:16 -0300

    Muda a referência a classe Node em ListaEncadeada

--- algoritmos/src/main/java/br/com/nca/algoritmos/listaencadeada/ListaEncadeada.java
+++ algoritmos/src/main/java/br/com/nca/algoritmos/listaencadeada/ListaEncadeada.java
@@ -1,6 +1,7 @@
 package br.com.nca.algoritmos.listaencadeada;
 
 import br.com.nca.algoritmos.exceptions.NumeroInteiroException;
+import br.com.nca.algoritmos.nodes.Node;
 
 public class ListaEncadeada {
 
