f82341cae48bc9d90e1180191b9ecdc05c3be455
alison <adevpr11@gmail.com>
2025-01-06 19:01:59 -0300

    Adiciona propriedades a classe Fila e construtor default

--- algoritmos/src/main/java/br/com/nca/algoritmos/fila/Fila.java
+++ algoritmos/src/main/java/br/com/nca/algoritmos/fila/Fila.java
@@ -1,4 +1,11 @@
 package br.com.nca.algoritmos.fila;
 
 public class Fila {
+    private int front;
+    private int back;
+    private int size;
+
+    public Fila() {
+        this.size = 0;
+    }
 }
