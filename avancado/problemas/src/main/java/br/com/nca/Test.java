a7d1f0a7965fcb172fd49b6bfabef43b4b90c14b
alison <adevpr11@gmail.com>
2025-01-14 22:33:07 -0300

    Remove comentários

--- avancado/problemas/src/main/java/br/com/nca/Test.java
+++ avancado/problemas/src/main/java/br/com/nca/Test.java
@@ -6,8 +6,8 @@ public class Test {
     public static void main(String[] args) {
         Fatorial fatorial = new Fatorial();
 
-        System.out.println("Recursivo:"+fatorial.recursivo(BigInteger.valueOf(500)));
-//        System.out.println("Top Down:"+fatorial.topDown(BigInteger.valueOf(110)));
-//        System.out.println("Bottom Up:"+fatorial.bottomUp(BigInteger.valueOf(110)));
+        System.out.println("Recursivo: "+fatorial.recursivo(BigInteger.valueOf(500)));
+        System.out.println("Top Down:"+fatorial.topDown(BigInteger.valueOf(110)));
+        System.out.println("Bottom Up:"+fatorial.bottomUp(BigInteger.valueOf(110)));
     }
 }
