c20a45a8688945870e6861d6dea7ebadbee75896
alison <adevpr11@gmail.com>
2025-02-18 18:34:47 -0300

    Commit inicial do exercício de testes

---
+++ codetest/src/main/java/br/com/nca/App.java
@@ -0,0 +1,13 @@
+package br.com.nca;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
